c86487da0c929d358dae8f17475db6962952c97d
Paul Baumgart <paul@baumgart.us>
2011-03-06 17:46:47 -0800

    Play a sound when pausing.

--- interface/beer-control.py
+++ interface/beer-control.py
@@ -1,10 +1,22 @@
 from SerialCommunicator import SerialCommunicator, DeviceSyncError
 from Recipe import Recipe
-import os, sys, time
+import os, ossaudiodev, sys, time, wave
 
 def usage():
     print 'Usage: python %s <recipe_file> <output_log_file>' % sys.argv[0]
     sys.exit(1)
+
+def play_sound(filename):
+    sound = wave.open(filename,'rb')
+    dsp = ossaudiodev.open('/dev/dsp','w')
+    dsp.setparameters(ossaudiodev.AFMT_S16_NE,
+                      sound.getnchannels(),
+                      sound.getframerate())
+    sound_data = sound.readframes(sound.getnframes())
+    sound.close()
+    dsp.write(sound_data)
+    dsp.close()
+
 
 try:
     recipe_file_name = sys.argv[1]
@@ -37,6 +49,7 @@ while True:
     current_status = s.read_current_status()
     #current_status = 'SPARGING,HEA,0,0,31.00,24.25,40.00,40.00,1000,ON'
     if current_status == 'PAUSED':
+        play_sound('flgalarm.wav')
         raw_input('Paused. Press Enter to continue.')
         s.serial.write('K')
     else:
